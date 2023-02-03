99651a6d693f12399bd95d8c694abbeb9f2cf5fb
mangji12 <71659709+mangji12@users.noreply.github.com>
2023-02-03 10:20:10 +0900

    Delete 1:6 directory

--- 1:6/PJT-01/01.py
+++
@@ -1,7 +0,0 @@
-with open('01.txt','w',encoding ='UTF8') as t:
-    t.write('Hello, Python!\n\
-1일차 파이썬 공부 중\n\
-2일차 파이썬 공부 중\n\
-3일차 파이썬 공부 중\n\
-4일차 파이썬 공부 중\n\
-5일차 파이썬 공부 중')
--- 1:6/PJT-01/02.py
+++
@@ -1,9 +0,0 @@
-with open('./PJT-01/data/fruits.txt','r') as t:
-    fruits = t.read()
-
-fruits = fruits.split('\n')
-count = len(fruits)
-
-index = open('02.txt','w')
-index.write(str(count))
-index.close()
